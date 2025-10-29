e6ead8d58fdf9437f9a3b99b09c1b3d329921a76
Your Name <you@example.com>
2025-10-29 12:17:48 +0300

    sql performance optimization

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,6 +1,9 @@
--- Task 4: Initial Complex Query
+-- Task 4: Optimized Complex Query with WHERE/AND
 -- Author: Amare Ushule
 -- Project: alx-airbnb-database
+
+-- Retrieve all bookings with user, property, and payment details
+-- Filter for active bookings in 2025 and completed payments
 
 SELECT 
     b.id AS booking_id,
@@ -14,9 +17,15 @@ SELECT
     pay.amount AS payment_amount,
     pay.payment_status
 FROM bookings b
-LEFT JOIN users u ON b.user_id = u.id
-LEFT JOIN properties p ON b.property_id = p.id
-LEFT JOIN payments pay ON b.id = pay.booking_id;
+LEFT JOIN users u 
+    ON b.user_id = u.id
+LEFT JOIN properties p 
+    ON b.property_id = p.id
+LEFT JOIN payments pay 
+    ON b.id = pay.booking_id
+WHERE b.start_date >= '2025-01-01'
+  AND b.end_date <= '2025-12-31'
+  AND (pay.payment_status = 'Completed' OR pay.payment_status IS NULL);
 
 EXPLAIN ANALYZE
 SELECT 
